a3c23d03d87a0aee5475411ec2c80f0fa44b562a
manwaner333 <manwaner333@gmail.com>
2024-06-15 15:27:00 +0400

    15_06_2024

--- Qing/safety_update/asr_eval.py
+++ Qing/safety_update/asr_eval.py
@@ -30,49 +30,73 @@ def get_our_evaluation(ques, resp):
 
 def eval_lvlm_defense(args):
     in_file = args.in_file
+    out_file = args.out_file
+
+    if os.path.exists(out_file):
+        os.remove(out_file)
 
     if args.dataset in ["vlguard", "safebench"]:
-        with open(in_file, "r", encoding="utf-8") as f:
-            content = f.readlines()
-            attack_sm_ll = [False for _ in range(len(content))]
-            attack_strong_ll = [0 for _ in range(len(content))]
-            for idx, line in enumerate(content):
-                json_data = json.loads(line)
-                ques = json_data["prompt"]
-                resp = json_data["response"]
+        with open(out_file, 'w') as file:
+            with open(in_file, "r", encoding="utf-8") as f:
+                content = f.readlines()
+                attack_sm_ll = [False for _ in range(len(content))]
+                attack_strong_ll = [0 for _ in range(len(content))]
+                for idx, line in enumerate(content):
+                    json_data = json.loads(line)
+                    prompt = json_data["prompt"]
+                    res = json_data["response"]
+                    id = json_data["id"]
+                    safe = json_data["safe"]
+                    harmful_category = json_data['harmful_category']
+                    harmful_subcategory = json_data["harmful_subcategory"]
+                    image_file = json_data["image_file"]
+
+                    if args.sm == "True":
+                        sm_score_1 = get_string_refusal(res)
+                        print(f"string_score:{not sm_score_1}")
+                        sm_score = not sm_score_1
+                        attack_sm_ll[idx] = sm_score
+
+                    if args.strong == "True":
+                        strong_score = get_strongreject(prompt, res)
+                        print(f"strong score: {strong_score}")
+                        attack_strong_ll[idx] = strong_score
+
+                    if args.debug == "True":
+                        if idx > 5:
+                            break
+
+                    output = {"idx": idx,
+                              'id': id,
+                              "safe": safe,
+                              "harmful_category": harmful_category,
+                              "harmful_subcategory": harmful_subcategory,
+                              "image_file": image_file,
+                              "prompt": prompt,
+                              "response": res,
+                              "sm_score": sm_score,
+                              "strong_score": strong_score
+                              }
+                    json.dump(output, file)
+                    file.write('\n')
+
 
                 if args.sm == "True":
-                    sm_score = get_string_refusal(resp)
-                    print(f"string_score:{not sm_score}")
-                    if not sm_score:
-                        attack_sm_ll[idx] = True
-                        print(resp)
+                    print(f"String Match ASR: {sum(attack_sm_ll)/len(attack_sm_ll)}")
+                    print(attack_sm_ll)
 
-
-                if args.strong == "True":
-                    strong_score = get_strongreject(ques, resp)
-                    print(f"strong score: {strong_score}")
-                    attack_strong_ll[idx] = strong_score
-
-                if args.debug == "True":
-                    if idx > 5:
-                        break
-
-            if args.sm == "True":
-                print(f"String Match ASR: {sum(attack_sm_ll)/len(attack_sm_ll)}")
-                print(attack_sm_ll)
-
-            elif args.strong == "True":
-                print(f"StrongReject Score:{sum(attack_strong_ll)/len(attack_strong_ll)}")
+                elif args.strong == "True":
+                    print(f"StrongReject Score:{sum(attack_strong_ll)/len(attack_strong_ll)}")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--in_file', type=str, default="result/safebench/llava-v1.5-7b_safebench_origin_model_without_images.json")
+    parser.add_argument('--in_file', type=str, default="result/safebench/llava-v1.5-7b_safebench_origin_model.json")
+    parser.add_argument('--out_file', type=str, default="result/safebench/llava-v1.5-7b_safebench_origin_model_annocated.json")
     parser.add_argument("--task", type=str, default="lvlm_defense")
     parser.add_argument("--dataset", type=str, default="safebench")
     parser.add_argument("--sm", type=str, default="True")
-    parser.add_argument("--strong", type=str, default="False")
+    parser.add_argument("--strong", type=str, default="True")
     parser.add_argument("--ours", type=str, default="False")
     parser.add_argument("--debug", type=str, default="False")
 
