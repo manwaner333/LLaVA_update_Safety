3c08e423504704a1893cddfa1e3d508b48cfa15f
manwaner333 <manwaner333@gmail.com>
2024-07-03 13:26:41 +0800

    03_07_2024

--- Qing/safety_intervention/generate_vectors_from_mass_mean_shift.py
+++ Qing/safety_intervention/generate_vectors_from_mass_mean_shift.py
@@ -105,8 +105,8 @@ def extract_block_activations(layers, activations_file="aa.json"):
 
 
 if __name__ == "__main__":
-    # save_path = "llava_v1.5_7b_vlguard_train_layer_level"
-    save_path = "llava_v1.5_7b_vlguard_train_head_level"
+    save_path = "llava_v1.5_13b_vlguard_train_layer_level"
+    # save_path = "llava_v1.5_7b_vlguard_train_head_level"
 
     if not os.path.exists(f"vectors/{save_path}"):
         os.mkdir(f"vectors/{save_path}")
@@ -115,30 +115,32 @@ if __name__ == "__main__":
         os.mkdir(f"clustering/{save_path}")
 
     start_layer = 0
-    end_layer = 31
+    end_layer = 2
     layers = list(range(start_layer, end_layer + 1))
 
     start_head = 0
     end_head = 31
     heads = list(range(start_head, end_head + 1))
 
-    answers_vlguard_pos_file = "playground/data/vlguard/answer_llava_v1.5_7b_vlguard_confrontational_cases_filter_pos.json"
-    answers_vlguard_neg_file = "playground/data/vlguard/answer_llava_v1.5_7b_vlguard_confrontational_cases_filter_neg.json"
+    # answers_vlguard_pos_file = "playground/data/vlguard/answer_llava_v1.5_7b_vlguard_confrontational_cases_filter_pos.json"
+    # answers_vlguard_neg_file = "playground/data/vlguard/answer_llava_v1.5_7b_vlguard_confrontational_cases_filter_neg.json"
+    answers_vlguard_pos_file = "playground/data/vlguard/answer_llava_v1.5_13b_vlguard_confrontational_cases_filter_pos.json"
+    answers_vlguard_neg_file = "playground/data/vlguard/answer_llava_v1.5_13b_vlguard_confrontational_cases_filter_neg.json"
     # layer level
-    # res_pos = extract_block_activations(layers, activations_file=answers_vlguard_pos_file)
-    # res_neg = extract_block_activations(layers, activations_file=answers_vlguard_neg_file)
+    res_pos = extract_block_activations(layers, activations_file=answers_vlguard_pos_file)
+    res_neg = extract_block_activations(layers, activations_file=answers_vlguard_neg_file)
     # head level
-    res_heads_pos = extract_block_heads_activations(layers, activations_file=answers_vlguard_pos_file)
-    res_heads_neg = extract_block_heads_activations(layers, activations_file=answers_vlguard_neg_file)
+    # res_heads_pos = extract_block_heads_activations(layers, activations_file=answers_vlguard_pos_file)
+    # res_heads_neg = extract_block_heads_activations(layers, activations_file=answers_vlguard_neg_file)
 
     for layer in layers:
         key = f"layer_{layer}"
         # layer level
-        # res_pos_sub = res_pos[key]
-        # res_neg_sub = res_neg[key]
+        res_pos_sub = res_pos[key]
+        res_neg_sub = res_neg[key]
         # head level
-        res_pos_sub = res_heads_pos[key]
-        res_neg_sub = res_heads_neg[key]
+        # res_pos_sub = res_heads_pos[key]
+        # res_neg_sub = res_heads_neg[key]
         positive = torch.tensor(np.array(res_pos_sub))
         negative = torch.tensor(np.array(res_neg_sub))
         vec = (positive - negative).mean(dim=0)
